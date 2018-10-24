d5db79438aa5f7a792e2b81469236a2b67a7e7d5
Arthur Dadalto <arthurpd10@gmail.com>
2018-10-24 12:22:12 -0300

    Adicionando calculo do coeficiente para o experimento de numero de clusters

--- exp1_kmeans.py
+++ exp1_kmeans.py
@@ -1,4 +1,5 @@
-import sklearn.cluster
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,14 +12,18 @@ test_set = pd.read_csv('test_set.csv')
 
 sse = []
 
-for i in range(1, 21):
-    kmeans = sklearn.cluster.KMeans(
+for i in range(2, 21):
+    kmeans = KMeans(
         n_clusters=i, n_init=10, max_iter=1000, n_jobs=-1).fit(train_set)
     print(kmeans.inertia_)
+    cluster_labels = kmeans.predict(train_set)
+    silhouette_avg = silhouette_score(train_set, cluster_labels)
+    print("For n_clusters =", i,
+          "The average silhouette_score is :", silhouette_avg)
     sse += [kmeans.inertia_]
 
 
-plt.plot(range(1, 21), sse, 'r-')
+plt.plot(range(2, 21), sse, 'r-')
 
 
 plt.show()
