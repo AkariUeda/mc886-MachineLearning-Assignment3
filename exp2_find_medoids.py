5920b81364ebb1058ac039b704466dc71301e82f
Akari <ueda.aka@gmail.com>
2018-10-30 11:26:12 -0300

    minor addaptations to exp2

--- exp2_find_medoids.py
+++ exp2_find_medoids.py
@@ -1,3 +1,16 @@
+'''
+
+Find the K centroids in the train set, print the N nearest medoids' neighbors
+and N random clusters' points.
+
+Parameters:
+
+clusters: number of clusters to train
+n_medoids: N nearest medoids' neighbors to print
+n_random: N random cluters' points to print
+
+'''
+
 import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans, DBSCAN, SpectralClustering
@@ -9,29 +22,16 @@ from scipy.spatial import distance
 clusters = int(sys.argv[1])
 n_medoids = int(sys.argv[2])
 n_random = int(sys.argv[3])
-datasetfile = sys.argv[4]
 
 train_set = pd.read_csv('train_set.csv')
 
 tweets = pd.read_csv('./dataset/health.txt', sep='|')
 
-test_tweets =  np.array(train_set, np.int32)[:,0]
+train_tweets =  np.array(train_set, np.int32)[:,0]
 
 train_set = np.array(train_set)[:,1:]
 
-scaler = StandardScaler()
-train_set = scaler.fit_transform(train_set, y=None)
-pca = PCA(n_components=0.90, svd_solver='full')
-train_set=pca.fit_transform(train_set)
-print(train_set.shape)
-
 clustering = KMeans(n_clusters=clusters, n_init=10, max_iter=10000, n_jobs=-1).fit(train_set)
-# number_of_clusters = max(labels)+1
-
-# clustering = DBSCAN(eps=0.5, min_samples=5).fit(train_set)
-# labels = clustering.labels_
-
-# clustering = SpectralClustering(n_clusters=clusters).fit(train_set)
 labels = clustering.labels_
 number_of_clusters = max(labels)+1
 
@@ -48,14 +48,14 @@ for c in range(number_of_clusters):
 	else:
 		n_neighbors = np.argpartition(dist_matrix[medoid],len(dist_matrix[medoid])-1)
 
-	print("\nMedoids cluster {} (size={}): {}".format(c,len(clusters[c]),tweets.iloc[test_tweets[idx_clusters[c][0][medoid]]]['headline_text']))
+	print("\nMedoids cluster {} (size={}): {}".format(c,len(clusters[c]),tweets.iloc[train_tweets[idx_clusters[c][0][medoid]]]['headline_text']))
 	print("   {} nearest points:".format(len(n_neighbors)))
 	for n in range(len(n_neighbors)):
-		print("          {}".format(tweets.iloc[test_tweets[idx_clusters[c][0][n_neighbors[n]]]]['headline_text']))
+		print("          {}".format(tweets.iloc[train_tweets[idx_clusters[c][0][n_neighbors[n]]]]['headline_text']))
 	
 	print("   {} random points:".format(n_random))
 	random_idx = np.random.randint(len(idx_clusters[c][0]),size=n_random)
 
 	for n in range(n_random):
-		print("          {}".format(tweets.iloc[test_tweets[idx_clusters[c][0][random_idx[n]]]]['headline_text']))
+		print("          {}".format(tweets.iloc[train_tweets[idx_clusters[c][0][random_idx[n]]]]['headline_text']))
 
